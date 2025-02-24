39b9883e58980fe7369ff1e6866bee4a5acd12ec
sethadenoIVYT <156937009+sethadenoIVYT@users.noreply.github.com>
2025-02-23 20:40:07 -0500

    Delete db.js

--- db.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://sdev255:Votto1931@songdb.k2wzc.mongodb.net/?retryWrites=true&w=majority&appName=SongDB", {useNewUrlParser: true})
-
-module.exports = mongoose
